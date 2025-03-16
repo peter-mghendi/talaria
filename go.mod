7c31ec3f40fdc9bdc0993878c51c71634ff1d6c3
peter-mghendi <plenjo15@gmail.com>
2025-03-16 15:32:44 +0300

    Implement email rendering

--- go.mod
+++ go.mod
@@ -8,4 +8,30 @@ require (
 	google.golang.org/protobuf v1.34.2
 )
 
-require golang.org/x/text v0.23.0 // indirect
+require (
+	github.com/Masterminds/goutils v1.1.1 // indirect
+	github.com/Masterminds/semver v1.5.0 // indirect
+	github.com/Masterminds/sprig v2.22.0+incompatible // indirect
+	github.com/PuerkitoBio/goquery v1.10.2 // indirect
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	github.com/aokoli/goutils v1.1.1 // indirect
+	github.com/go-hermes/hermes/v2 v2.3.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/css v1.0.1 // indirect
+	github.com/huandu/xstrings v1.5.0 // indirect
+	github.com/imdario/mergo v0.3.16 // indirect
+	github.com/jaytaylor/html2text v0.0.0-20230321000545-74c2419ad056 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/ssor/bom v0.0.0-20170718123548-6386211fdfcf // indirect
+	github.com/vanng822/css v1.0.1 // indirect
+	github.com/vanng822/go-premailer v1.23.0 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+)
--- internal/render/service.go
+++ internal/render/service.go
@@ -1,10 +1,107 @@
 package service
 
-import renderv1 "github.com/peter-mghendi/talaria/internal/gen/render/v1"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/go-hermes/hermes/v2"
+	renderv1 "github.com/peter-mghendi/talaria/internal/gen/render/v1"
+)
 
 func Render(request *renderv1.RenderRequest) renderv1.RenderResponse {
-	return renderv1.RenderResponse{
-		Html: "<p>Hey there!</p>",
-		Text: "Hey there!",
+	hermes := ConvertProtoToHermes(request.Hermes)
+	email := ConvertProtoToEmail(request.Email)
+
+	html, _ := hermes.GenerateHTML(email)
+	text, _ := hermes.GeneratePlainText(email)
+
+	return renderv1.RenderResponse{Html: html, Text: text}
+}
+
+// ConvertProtoToHermes converts renderv1.Hermes to hermes.Hermes
+func ConvertProtoToHermes(h *renderv1.Hermes) hermes.Hermes {
+	return hermes.Hermes{
+		Theme:              GetTheme(&h.Theme),
+		TextDirection:      hermes.TextDirection(h.TextDirection),
+		DisableCSSInlining: h.DisableCssInlining,
+		Product: hermes.Product{
+			Name:        h.Product.Name,
+			Link:        h.Product.Link,
+			Logo:        h.Product.Logo,
+			Copyright:   h.Product.Copyright,
+			TroubleText: h.Product.TroubleText,
+		},
+	}
+}
+
+// ConvertProtoToEmail converts renderv1.Email to hermes.Email
+func ConvertProtoToEmail(e *renderv1.Email) hermes.Email {
+	var tables []hermes.Table
+	for _, t := range e.Body.Tables {
+		var tableData [][]hermes.Entry
+		for _, entry := range t.Data {
+			tableData = append(tableData, []hermes.Entry{
+				{
+					Key:   entry.Key,
+					Value: entry.Value,
+				},
+			})
+		}
+		tables = append(tables, hermes.Table{
+			Title: t.Title,
+			Data:  tableData,
+		})
+	}
+
+	var actions []hermes.Action
+	for _, a := range e.Body.Actions {
+		actions = append(actions, hermes.Action{
+			Instructions: a.Instructions,
+			InviteCode:   a.InviteCode,
+			Button: hermes.Button{
+				Color:     a.Button.Color,
+				TextColor: a.Button.TextColor,
+				Text:      a.Button.Text,
+				Link:      a.Button.Link,
+			},
+		})
+	}
+
+	return hermes.Email{
+		Body: hermes.Body{
+			Name:      e.Body.Name,
+			Intros:    e.Body.Intros,
+			Outros:    e.Body.Outros,
+			Greeting:  e.Body.Greeting,
+			Signature: e.Body.Signature,
+			Title:     e.Body.Title,
+			Dictionary: func() []hermes.Entry {
+				var entries []hermes.Entry
+				for _, entry := range e.Body.Dictionary {
+					entries = append(entries, hermes.Entry{
+						Key:   entry.Key,
+						Value: entry.Value,
+					})
+				}
+				return entries
+			}(),
+			Tables:  tables,
+			Actions: actions,
+		},
+	}
+}
+
+func GetTheme(name *string) hermes.Theme {
+	if name == nil {
+		return new(hermes.Default)
+	}
+
+	switch theme := strings.ToLower(strings.TrimSpace(*name)); theme {
+	case "default":
+		return new(hermes.Default)
+	case "flat":
+		return new(hermes.Flat)
+	default:
+		panic(fmt.Errorf("unknown theme: %s", theme))
 	}
 }
